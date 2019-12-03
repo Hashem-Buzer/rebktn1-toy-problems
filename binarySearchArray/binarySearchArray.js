e298c215401dc884a73807c2e51acf73ffed6292
hashem-buzer <hbuzer98@gmail.com>
2019-12-03 09:07:10 +0100

    done

---
+++ binarySearchArray/binarySearchArray.js
@@ -0,0 +1,20 @@
+/*
+ * Given a sorted array, find the index of an element
+ * using a binary search algorithm.
+ *
+ * Example usage:
+ *
+ * var index = binarySearch([1, 2, 3, 4, 5], 4);
+ * console.log(index); // 3
+ * var index = binarySearch([1, 2, 3, 4, 5], 8);
+ * console.log(index); // null
+ */
+
+var binarySearch = function (array, target) {
+	
+	if (target > array.length) {
+		return null;
+	} else if (array[target - 1] === target) {
+			return array.indexOf(array[target - 1]);
+	}
+};
