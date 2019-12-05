20fc69e2ecccdacde9485ae3841f5eca5e43c869
hashem-buzer <hbuzer98@gmail.com>
2019-12-05 09:58:15 +0100

    done

---
+++ characterFrequency/CharacterFrequency.js
@@ -0,0 +1,41 @@
+/*
+Character Frequency
+Write a function call characterFrequency that takes as its input a string and returns an array
+of arrays as shown below sorted in descending order by frequency
+and then by ascending order by character.
+
+
+characterFrequency("aaabbc") -->    [ [ "a", 3 ], [ "b", 2 ], [ "c", 1 ] ]
+
+characterFrequency("mississippi")  -->  [ [ "i", 4 ], [ "s", 4 ], [ "p", 2 ], [ "m", 1 ] ]
+
+characterFrequency("") -->  [ ]
+*/
+
+function characterFrequency(str) {
+	var string = str.split('')
+	var array = [];
+	var no = 0;
+	var char;
+	var count = 0;
+	var newArr = [];
+
+	for (var i = 0; i < string.length; i++) {
+		char = string[no];
+		for (var j = 0; j < string.length; j++) {
+			if (char === string[j]) {
+				count++;
+				no++;
+				// console.log(newArr)
+			}
+		}
+				newArr.push(char, count);
+				console.log(char)
+				array.push(newArr)
+				count = 0;
+	}
+
+	return array
+}
+
+// half way correct except the undefined i tried to solve it but i couldnt
