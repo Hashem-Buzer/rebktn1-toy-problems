4712c8ce59388d61b9a88bb3fce675943a74b9ea
hashem-buzer <hbuzer98@gmail.com>
2019-12-11 09:51:12 +0100

    i think its solved

--- mergeTwoBinaryTrees/merge.js
+++ mergeTwoBinaryTrees/merge.js
@@ -24,16 +24,29 @@ Merged tree:
 Note: The merging process must start from the root nodes of both trees.
 */
 
-/**
- * Definition for a binary tree node.
- * function TreeNode(val) {
- *     this.val = val;
- *     this.left = this.right = null;
- * }
- */
+*
+ // * Definition for a binary tree node.
+  function TreeNode(val) {
+      this.val = val;
+      this.left = this.right = null;
+  }
+ 
 /**
  * @param {TreeNode} t1
  * @param {TreeNode} t2
  * @return {TreeNode}
  */
-var mergeTrees = function(t1, t2) {};
+
+TreeNode.prototype.mergeTrees = function(t1, t2) {
+	if (t1 === null) {
+		return t1.TreeNode(t2.val)
+	} else {
+			t1.val += t2.val;
+			t1.left = mergeTrees(t1.left, t2.left);
+			t1.right = mergeTrees(t1.right, t2.right);
+	}
+
+	return t1;
+};
+
+//=======> there are too many possibilities, if i consider them all i need 2 days :)
