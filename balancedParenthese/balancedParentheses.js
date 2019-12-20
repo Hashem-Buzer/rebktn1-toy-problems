45eb0f71713f5dbea967b9fcd100b86b8cc1a237
hashem-buzer <hbuzer98@gmail.com>
2019-12-20 09:15:29 +0100

    done

--- balancedParenthese/balancedParentheses.js
+++ balancedParenthese/balancedParentheses.js
@@ -8,4 +8,30 @@ isBalanced("(50)(")	// false
 isBalanced("") //	true
 */
 
-var isBalanced = function(str) {};
+function isBalanced(char) {
+	var char = char.split('');
+	var stack = [];
+  var types = {
+  	'(': ')',
+    '[': ']',
+  	'{': '}'
+  }
+
+  for (var i = 0; i < char.length; i++) {
+  	if (char[i] === '(' || char[i] === '{' || char[i] === '[' ) {
+    	stack.push(char[i]);
+     } else {
+       var poped = stack.pop();
+
+       if (char[i] === types[poped]) {
+        return true
+      };
+    }
+  }
+  
+  if (stack.length !== 0) {
+  	return false
+  };
+
+    return true;
+};
