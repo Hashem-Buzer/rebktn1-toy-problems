d396b5596031ac093b378a488a7e5ec394d86b2c
hashem-buzer <hbuzer98@gmail.com>
2019-12-24 09:43:29 +0100

    alomost cool

--- balancedBrackets/balancedBrackets.js
+++ balancedBrackets/balancedBrackets.js
@@ -9,4 +9,31 @@ isBalanced("(50)(") // false
 isBalanced("[{]}") // false
 */
 
-var isBalanced = function(str) {};
+	var isBalanced = function(str) {
+		var stack = [];
+		var bool = false;
+	  var types = {
+	    '[': ']',
+	  	'(': ')',
+	  	'{': '}'
+	  }
+
+	  for (var i = 0; i < str.length; i++) {
+	  	if (str[i] === '(' || str[i] === '{' || str[i] === '[' ) {
+	    	stack.push(str[i]);
+	     } else {
+	       var poped = stack.pop();
+
+	       if (str[i] === types[poped]) {
+	        bool = true
+	      };
+	    }
+	  }
+	  
+	  if (stack.length !== 0) {
+	  	bool = false
+	  } else {
+	  	bool = true;
+	  }
+	  return bool
+	};
