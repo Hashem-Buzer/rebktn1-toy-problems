0d7c61657f193f95627b57477f9bbaf74f8140a2
hashem-buzer <hbuzer98@gmail.com>
2019-12-26 09:45:11 +0100

    not solved

--- allAnagrams/allAnagrams.js
+++ allAnagrams/allAnagrams.js
@@ -14,4 +14,22 @@
 
 var allAnagrams = function(string) {
   // Your code here.
+  // var arr = [];
+  // var str = string.split('');
+
+  // for (var i = 0; i < str.length; i++) {
+  // 	var char = str.pop();
+  // 	str.unshift(char);
+	 //  // console.log(str)
+  // }
+
+  // for (var i = 0; i < Math.pow(str.length, 2); i++) {
+  // 	var char = str.slice(str.length - 2);
+  // 	str.unshift(char);
+	 //  console.log(str)
+  	
+  // }
+
+
+  // DO NOT RUN THIS CODE
 };
