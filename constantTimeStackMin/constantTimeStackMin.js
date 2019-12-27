8b73a7aa14c2325e6237ebf6b67e07baa967fbc5
hashem-buzer <hbuzer98@gmail.com>
2019-12-27 09:27:14 +0100

    done

--- constantTimeStackMin/constantTimeStackMin.js
+++ constantTimeStackMin/constantTimeStackMin.js
@@ -21,20 +21,65 @@
 
   // add an item to the top of the stack
     this.push = function(value) {
+	    this.min = min || value;
+
+		 	if (value < this.min) {
+		 		this.min = value
+		 	}
+		  this.storage[++this.top] = value;
     };
 
   // remove an item from the top of the stack
     this.pop = function() {
+		  var top = this.storage[this.top];
+		  delete this.storage[this.top--];
+		  
+		  return top;
     };
 
   // return the number of items in the stack
     this.size = function() {
+	  	return Object.keys(this).length;
     };
   
   // return the minimum value in the stack
     this.min = function() {
-
+  		return this.min;
     };
 
   };
 
+
+ //  var Stack = function() {
+ //  	this.min;
+ //  	this.top = -1;
+ //  	this.storage = {}; 
+	// };
+
+ //  // add an item to the top of the stack
+	//  Stack.prototype.push = function(value) {
+	//  	this.min = min || value;
+	//  	if (value < this.min) {
+	//  		this.min = value
+	//  	}
+	//   this.storage[++this.top] = value;
+	// };
+
+ //  // remove an item from the top of the stack
+	// Stack.prototype.pop = function() {
+
+	//   var top = this.storage[this.top];
+	//   delete this.storage[this.top--];
+	  
+	//   return top;
+	// };
+
+ //  // return the number of items in the stack
+	// Stack.prototype.size = function() {
+	//   return Object.keys(this.storage).length;
+	// };
+
+ //  // return the minimum value in the stack
+ //  Stack.prototype.min = function() {
+ //  	return this.min;
+ // 	};
