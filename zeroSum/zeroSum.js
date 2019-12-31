b937348e5baa21dbec8df503d18618f13de6e0e6
hashem-buzer <hbuzer98@gmail.com>
2019-12-31 09:13:12 +0100

    done

--- zeroSum/zeroSum.js
+++ zeroSum/zeroSum.js
@@ -7,3 +7,19 @@ Constraints: Solve this linear time complexity O(n)
 zeroSum([ 1, 3, 2, -3 ]) // true
 zeroSum([ 5, 7, 2, 9 ]) // false
 */
+function zeroSum(array) {
+	var n = -1;
+	var first = array[n] 
+	var bool;
+	
+	for (var i = 1; i <= array.length - 1; i++) {
+		// console.log(array[i] + array[n], array[n], array[i])
+		if (array[i] + array[n] === 0) {
+			bool = true
+		} else {
+			bool = false;
+		}
+			n++;
+	}
+	return bool;
+}
